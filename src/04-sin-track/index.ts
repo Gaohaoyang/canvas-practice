849b6a8984a03b9cdfbe6ac8567ec2e619cf592c
chuanshi <ghy118432@alibaba-inc.com>
2021-07-12 01:40:08 +0800

    fix: 🐛 sin track

---
+++ src/04-sin-track/index.ts
@@ -0,0 +1,32 @@
+import Ball from '../common/Ball'
+
+const canvas: HTMLCanvasElement | null = document.querySelector('#mainCanvas')
+
+if (canvas) {
+  canvas.width = 800
+  canvas.height = 400
+  const context = canvas.getContext('2d')
+
+  if (context) {
+    const ball = new Ball(1)
+    ball.y = canvas.height
+    ball.x = 0
+    const speedY = 2
+    const speedX = 50
+
+    let angle = 0
+
+    let then = 0
+    const drawFrame = (time: number) => {
+      const timeInSeconds = time * 0.001
+      const deltaTimeInSeconds = timeInSeconds - then
+      then = timeInSeconds
+      ball.x += speedX * deltaTimeInSeconds
+      ball.y = canvas.height / 2 + Math.sin(angle) * 50
+      angle += speedY * deltaTimeInSeconds
+      ball.draw(context)
+      window.requestAnimationFrame(drawFrame)
+    }
+    drawFrame(0)
+  }
+}
