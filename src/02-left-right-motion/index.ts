af95dfaf4496f436497381880d028aa86a1a3991
chuanshi <ghy118432@alibaba-inc.com>
2021-07-12 01:03:10 +0800

    feat: 🎸 02-left-right-motion

---
+++ src/02-left-right-motion/index.ts
@@ -0,0 +1,25 @@
+import Ball from '../common/Ball'
+
+const canvas: HTMLCanvasElement | null = document.querySelector('#mainCanvas')
+
+if (canvas) {
+  canvas.width = 800
+  canvas.height = 400
+  const context = canvas.getContext('2d')
+
+  if (context) {
+    const ball = new Ball()
+    ball.x = canvas.width / 2
+    ball.y = canvas.height / 2
+    let angle = 0
+
+    const drawFrame = () => {
+      window.requestAnimationFrame(drawFrame)
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      ball.x = canvas.width / 2 + Math.sin(angle) * 50
+      angle += 0.1
+      ball.draw(context)
+    }
+    drawFrame()
+  }
+}
---
+++ src/common/Ball.ts
@@ -0,0 +1,39 @@
+class Ball {
+  radius: number
+
+  color: string
+
+  x: number
+
+  y: number
+
+  lineWidth: number
+
+  constructor(radius: number = 40, color: string = '#795548') {
+    this.radius = radius
+    this.color = color
+    this.x = 0
+    this.y = 0
+    this.lineWidth = 1
+  }
+
+  /**
+   * draw
+   */
+  public draw(context: CanvasRenderingContext2D) {
+    context.save()
+    context.translate(this.x, this.y)
+    context.lineWidth = this.lineWidth
+    context.fillStyle = this.color
+    context.beginPath()
+    context.arc(0, 0, this.radius, 0, Math.PI * 2, true)
+    context.closePath()
+    context.fill()
+    if (this.lineWidth > 0) {
+      context.stroke()
+    }
+    context.restore()
+  }
+}
+
+export default Ball
