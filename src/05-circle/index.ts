5239176441ea5ced48f5399c0fb2799ef2841e07
chuanshi <ghy118432@alibaba-inc.com>
2021-07-13 11:52:49 +0800

    feat: 🎸 circle

---
+++ src/05-circle/index.ts
@@ -0,0 +1,37 @@
+import Ball from '../common/Ball'
+
+const canvas: HTMLCanvasElement | null = document.querySelector('#mainCanvas')
+
+if (canvas) {
+  canvas.width = 800
+  canvas.height = 400
+  const context = canvas.getContext('2d')
+
+  if (context) {
+    const ball = new Ball(5)
+    const ball2 = new Ball(50, '#3949AB')
+    ball.x = canvas.width / 2
+    ball.y = canvas.height / 2
+    ball2.x = canvas.width / 2
+    ball2.y = canvas.height / 2
+    const speed = 2
+    const r = 100
+
+    let angle = 0
+
+    let then = 0
+    const drawFrame = (time: number) => {
+      context.clearRect(0, 0, canvas.width, canvas.height)
+      const timeInSeconds = time * 0.001
+      const deltaTimeInSeconds = timeInSeconds - then
+      then = timeInSeconds
+      ball.x = canvas.width / 2 + r * Math.cos(angle)
+      ball.y = canvas.height / 2 + r * Math.sin(angle)
+      angle += speed * deltaTimeInSeconds
+      ball.draw(context)
+      ball2.draw(context)
+      window.requestAnimationFrame(drawFrame)
+    }
+    drawFrame(0)
+  }
+}
