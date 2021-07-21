5c5dac633a36e68aa07136acbbffb1c808733f9d
chuanshi <ghy118432@alibaba-inc.com>
2021-07-22 00:21:11 +0800

    feat: 🎸 uuuu

--- src/16-image-pixel/index.ts
+++ src/16-image-pixel/index.ts
@@ -4,8 +4,7 @@ if (canvas) {
   const ctx = canvas.getContext('2d')
   if (ctx) {
     const img = new Image()
-    img.src =
-      'https://gw.alicdn.com/imgextra/i2/O1CN01gR6ymq1dfV5RmYxYk_!!6000000003763-2-tps-658-411.png'
+    img.src = 'https://gw.alicdn.com/imgextra/i2/O1CN01gR6ymq1dfV5RmYxYk_!!6000000003763-2-tps-658-411.png'
     img.addEventListener('load', () => {
       ctx.drawImage(img, 0, 0, 658, 329, 0, 0, canvas.width, canvas.height)
       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
