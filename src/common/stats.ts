c4cbca2bafc3735a99975b7cd84366ecdeee4375
chuanshi <ghy118432@alibaba-inc.com>
2021-07-24 18:40:19 +0800

    feat: 🎸 move

--- src/common/stats.ts
+++ src/common/stats.ts
@@ -2,7 +2,8 @@ import Stats from 'stats.js'
 
 const stats = new Stats()
 stats.dom.style.left = 'auto'
-stats.dom.style.right = '0'
+stats.dom.style.top = '10px'
+stats.dom.style.right = '10px'
 document.body.appendChild(stats.dom)
 
 export default stats
